829c681790ac48f630888de5b39e51c917f532ee
Mamia Hossain Nodi <120479432+Mamia123@users.noreply.github.com>
2024-11-25 13:38:05 +0600

    1st commit

---
+++ midlabexam/homepage.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+if(!(isset($_SESSTON["stATUS]")))
+                                 {
+                                  header("location:login.html");
+								 }
+?>								  
+			
+			
+			
+			
+			
+<html>
+    <head>
+              <title>Homepage</title>
+    </head> 
+	<body>
+        <?php echo "Hi!  {$_SESSION["username"]}" ?>
+        <p><b>Welcome to the homepage!</b></p>
+        <a href ="logout.php">Log Out</a>
+    </body>
+</html>
---
+++ midlabexam/login_check.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+if(isset($_post["submit"])){
+	                        $login_name = $_POST["name"];
+                            $login_password = $_POST["password"];
+		if(empty($login_name) || empty($login_password)){ 
+                                                        	echo "Username or password can not be empty";
+                                                        }	
+     		else if(( isset($_SESSION["username"]) && isset($_SESSION["password"]) && $login_name === $_SESSION["username"]) && ($login_password === $_SESSION["password"])){  
+                     			    $_SESSION["status"] = true;
+                                   header("location: home.php");}
+				    else{
+                            echo "username and password is not valid";}
+                               }
+                             else{
+                                   header("location:login.html");}
+
+?>
